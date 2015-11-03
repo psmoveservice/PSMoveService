832f2cca91ce511e69ebf26e50ba1febcdef6c7b
Chadwick Boulay <chadwick.boulay@gmail.com>
2015-11-03 10:38:57 -0500

    Simple json reading/writing is done. See tests/test_config.cpp for an example of how to use.

--- psmoveservice/PSMoveConfig/PSMoveConfig.cpp
+++ psmoveservice/PSMoveConfig/PSMoveConfig.cpp
@@ -5,30 +5,47 @@
 
 #include <iostream>
 
-/* System-wide data directory */
-#define SYSTEM_DATA_DIR "/etc/psmoveservice"
-#ifdef _WIN32
-#define ENV_USER_HOME "APPDATA"
-#else
-#define ENV_USER_HOME "HOME"
-#endif
-
-PSMoveConfig::PSMoveConfig()
+PSMoveConfig::PSMoveConfig(const std::string &fnamebase)
+: ConfigFileBase(fnamebase)
 {
-    boost::property_tree::ptree tree;
-    //boost::property_tree::read_json(<YOUR PATH TO AND FILE NAME HERE>, tree);
-    const char *homedir = getenv(ENV_USER_HOME);
-#ifndef _WIN32
-    // if run as root, use system-wide data directory
-    if (geteuid() == 0) {
-        homedir = SYSTEM_DATA_DIR;
-    }
-#endif
-	boost::filesystem::path configpath(homedir);
-	std::cout << "HomeDir: " << configpath << std::endl;
 }
 
-PSMoveConfig::~PSMoveConfig()
+const std::string
+PSMoveConfig::getConfigPath()
 {
+    const char *homedir;
+#ifdef _WIN32
+    homedir = getenv("APPDATA");
+#else
+    homedir = getenv("HOME");
+    // if run as root, use system-wide data directory
+    if (geteuid() == 0) {
+        homedir = "/etc/psmoveservice";
+    }
+#endif
     
+    boost::filesystem::path configpath(homedir);
+    configpath /= "PSMoveService";
+    boost::filesystem::create_directory(configpath);
+    configpath /= ConfigFileBase + ".json";
+    std::cout << "Config file name: " << configpath << std::endl;
+    return configpath.string();
+}
+
+void
+PSMoveConfig::save()
+{
+    boost::property_tree::write_json(getConfigPath(), config2ptree());
+}
+
+void
+PSMoveConfig::load()
+{
+    boost::property_tree::ptree pt;
+    std::string configPath = getConfigPath();
+    if ( boost::filesystem::exists( configPath ) )
+    {
+        boost::property_tree::read_json(configPath, pt);
+        ptree2config(pt);
+    }
 }
--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -1,7 +1,57 @@
 #include "PSMoveConfig.h"
+#include <iostream>
+
+// Function definitions would go in class .cpp
+class MyConfig: public PSMoveConfig
+{
+public:
+    MyConfig(const std::string &fnamebase = "MyConfig"): PSMoveConfig(fnamebase){}
+    
+    int myInt = 23;
+    float myFloat = 18.02f;
+    std::string myString = "class string";
+    struct myStructType
+    {
+        float x = 0.1f;
+        float y = -0.2f;
+        float z = 0.3f;
+    } myStruct;
+
+    virtual const boost::property_tree::ptree
+    config2ptree()
+    {
+        boost::property_tree::ptree pt;
+        pt.put("myInt", myInt);
+        pt.put("myFloat", myFloat);
+        pt.put("myString", myString);
+        pt.put("myStruct.x", myStruct.x);
+        pt.put("myStruct.y", myStruct.y);
+        pt.put("myStruct.z", myStruct.z);
+        
+        return pt;
+    }
+    
+    virtual void
+    ptree2config(const boost::property_tree::ptree &pt)
+    {
+        myInt = pt.get<int>("myInt", 0);
+        myFloat = pt.get<float>("myFloat", 0.0f);
+        myString = pt.get<std::string>("myString", "default string");
+        myStruct.x = pt.get<float>("myStruct.x", 0.0f);
+        myStruct.y = pt.get<float>("myStruct.y", 0.0f);
+        myStruct.z = pt.get<float>("myStruct.z", 0.0f);
+    }
+};
 
 int main()
 {
-	PSMoveConfig myConfig;
+    MyConfig myConfig("test_config");
+    std::cout << "Default myInt = " << myConfig.myInt << std::endl;
+    myConfig.load();  // Note we could load() in the constructor but then we couldn't print the default value.
+    std::cout << "Loaded myInt = " << myConfig.myInt << std::endl;
+    myConfig.myInt = 40;
+    myConfig.save();
     return 0;
+    
+    // Try editing the json and running again.
 }
