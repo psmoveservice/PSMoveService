768e0c81c6f9992764fc9fb92db98778c3f9fb94
Brendan Walker <brendan@millerwalker.net>
2015-11-02 17:22:17 -0800

    Got boost_service branch building with cmake generated projects again * Had to switch boost over to using static c-runtime (not sure if this is ok long term) * Had to switch psmoveservice and psmovedataframe over to using static c-runtime (not sure if this is ok long term) * Got rid of un-needed FindBoost.cmake and FindOpenCV.cmake scripts (come with cmake already) * Got rid of ExampleServerMain.cpp (didn't mean to check this in) * Fixed psmoveservice project to be dependent on psmovedataframe * Fixed target link libraries for libprotobuf

--- RunCMake.bat
+++ RunCMake.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake .. -G "Visual Studio 12" -DOpenCV_DIR=E:\OpenCV-3.0.0\build -DBOOST_ROOT=E:\boost_1_59_0 -DBOOST_LIBRARYDIR=E:\boost_1_59_0\lib32-msvc-12.0
+cmake .. -G "Visual Studio 12" -DOpenCV_DIR=C:\OpenCV-3.0.0\build -DBOOST_ROOT=C:\boost_1_59_0 -DBOOST_LIBRARYDIR=C:\boost_1_59_0\lib32-msvc-12.0
 pause
