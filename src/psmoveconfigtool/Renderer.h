a7c5882bcb15bf7b23e4cb574fbfffdabc58f22a
brendanwalker <brendan@millerwalker.net>
2016-02-13 01:16:26 -0800

    Added drawEllipsoid used to render an ellipsoid

--- src/psmoveconfigtool/Renderer.h
+++ src/psmoveconfigtool/Renderer.h
@@ -82,6 +82,10 @@ void drawTransformedBox(const glm::mat4 &transform, const glm::vec3 &half_extent
 void drawTransformedBox(const glm::mat4 &transform, const glm::vec3 &box_min, const glm::vec3 &box_max, const glm::vec3 &color);
 void drawTransformedTexturedCube(const glm::mat4 &transform, int textureId, float scale);
 void drawPointCloud(const glm::mat4 &transform, const glm::vec3 &color, const float *points, const int point_count);
+void drawEllipsoid(
+    const glm::mat4 &transform, const glm::vec3 &color, 
+    const glm::mat3 &basis, const glm::vec3 &center, const glm::vec3 &extents,
+    const int subdiv= 64);
 void drawLineStrip(const glm::mat4 &transform, const glm::vec3 &color, const float *points, const int point_count);
 void drawDK2Model(const glm::mat4 &transform);
 void drawPSMoveModel(const glm::mat4 &transform, const glm::vec3 &color);
