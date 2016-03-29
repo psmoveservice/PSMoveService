04626030ad9f43bb40bd8900c73b9947ba139ecf
brendanwalker <brendan@millerwalker.net>
2016-03-29 00:25:58 -0700

    First pass at adding HMD list display on the config client.

--- src/psmoveconfigtool/AppStage_HMDSettings.cpp
+++ src/psmoveconfigtool/AppStage_HMDSettings.cpp
@@ -5,6 +5,8 @@
 #include "Camera.h"
 #include "Renderer.h"
 #include "UIConstants.h"
+#include "PSMoveProtocolInterface.h"
+#include "PSMoveProtocol.pb.h"
 
 #include "SDL_keycode.h"
 
@@ -19,11 +21,16 @@ const char *AppStage_HMDSettings::APP_STAGE_NAME= "HMDSettings";
 //-- public methods -----
 AppStage_HMDSettings::AppStage_HMDSettings(App *app) 
     : AppStage(app)
+    , m_menuState(AppStage_HMDSettings::inactive)
+    , m_selectedHmdIndex(-1)
 { }
 
 void AppStage_HMDSettings::enter()
 {
     m_app->setCameraType(_cameraFixed);
+    m_selectedHmdIndex = -1;
+
+    request_hmd_list();
 }
 
 void AppStage_HMDSettings::exit()
@@ -36,24 +43,221 @@ void AppStage_HMDSettings::update()
     
 void AppStage_HMDSettings::render()
 {
-    drawDK2Model(glm::mat4(1.f));
+    switch (m_menuState)
+    {
+    case eHmdMenuState::idle:
+    {
+        if (m_selectedHmdIndex >= 0)
+        {
+            const HMDInfo &hmdInfo = m_hmdInfos[m_selectedHmdIndex];
+
+            switch (hmdInfo.HmdType)
+            {
+            case PSMoveProtocol::OculusDK2:
+                {
+                    glm::mat4 scale3 = glm::scale(glm::mat4(1.f), glm::vec3(3.f, 3.f, 3.f));
+                    drawDK2Model(scale3);
+                } break;
+            default:
+                assert(0 && "Unreachable");
+            }
+        }
+    } break;
+
+    case eHmdMenuState::pendingHmdListRequest:
+    case eHmdMenuState::failedHmdListRequest:
+        {
+        } break;
+
+    default:
+        assert(0 && "unreachable");
+    }
 }
 
 void AppStage_HMDSettings::renderUI()
 {
-    ImGuiWindowFlags window_flags = 
+    const char *k_window_title = "Tracker Settings";
+    const ImGuiWindowFlags window_flags =
         ImGuiWindowFlags_ShowBorders |
-        ImGuiWindowFlags_NoResize | 
+        ImGuiWindowFlags_NoResize |
         ImGuiWindowFlags_NoMove |
         ImGuiWindowFlags_NoScrollbar |
         ImGuiWindowFlags_NoCollapse;
-    ImGui::SetNextWindowPosCenter();
-    ImGui::Begin("HMD Settings", nullptr, ImVec2(300, 150), k_background_alpha, window_flags);
 
-    if (ImGui::Button("Return to Main Menu"))
+    switch (m_menuState)
     {
-        m_app->setAppStage(AppStage_MainMenu::APP_STAGE_NAME);
+    case eHmdMenuState::idle:
+    {
+        ImGui::SetNextWindowPosCenter();
+        ImGui::SetNextWindowSize(ImVec2(300, 400));
+        ImGui::Begin(k_window_title, nullptr, window_flags);
+
+        if (m_hmdInfos.size() > 0)
+        {
+            const HMDInfo &hmdInfo = m_hmdInfos[m_selectedHmdIndex];
+
+            ImGui::Text("HMD: %d", m_selectedHmdIndex);
+            ImGui::Text("  HMD ID: %d", hmdInfo.HmdID);
+
+            switch (hmdInfo.HmdType)
+            {
+            case AppStage_HMDSettings::OculusDK2:
+            {
+                ImGui::Text("  HMD Type: Oculus DK2");
+            } break;
+            default:
+                assert(0 && "Unreachable");
+            }
+
+            ImGui::TextWrapped("  Device Path: %s", hmdInfo.DevicePath.c_str());
+
+            if (m_selectedHmdIndex > 0)
+            {
+                if (ImGui::Button("Previous HMD"))
+                {
+                    --m_selectedHmdIndex;
+                }
+            }
+
+            if (m_selectedHmdIndex + 1 < static_cast<int>(m_hmdInfos.size()))
+            {
+                if (ImGui::Button("Next HMD"))
+                {
+                    ++m_selectedHmdIndex;
+                }
+            }
+
+            if (ImGui::Button("Test HMD Tracking"))
+            {
+                //m_app->setAppStage(AppStage_TestHMD::APP_STAGE_NAME);
+            }
+        }
+        else
+        {
+            ImGui::Text("No HMDs");
+        }
+
+        if (ImGui::Button("Return to Main Menu"))
+        {
+            m_app->setAppStage(AppStage_MainMenu::APP_STAGE_NAME);
+        }
+
+        ImGui::End();
+    } break;
+    case eHmdMenuState::pendingHmdListRequest:
+    {
+        ImGui::SetNextWindowPosCenter();
+        ImGui::SetNextWindowSize(ImVec2(300, 150));
+        ImGui::Begin(k_window_title, nullptr, window_flags);
+
+        ImGui::Text("Waiting for HMD list response...");
+
+        ImGui::End();
+    } break;
+    case eHmdMenuState::failedHmdListRequest:
+    {
+        ImGui::SetNextWindowPosCenter();
+        ImGui::SetNextWindowSize(ImVec2(300, 150));
+        ImGui::Begin(k_window_title, nullptr, window_flags);
+
+        ImGui::Text("Failed to get tracker list!");
+
+        if (ImGui::Button("Retry"))
+        {
+            request_hmd_list();
+        }
+
+        if (ImGui::Button("Return to Main Menu"))
+        {
+            m_app->setAppStage(AppStage_MainMenu::APP_STAGE_NAME);
+        }
+
+        ImGui::End();
+    } break;
+
+    default:
+        assert(0 && "unreachable");
+    }
+}
+
+bool AppStage_HMDSettings::onClientAPIEvent(
+    ClientPSMoveAPI::eClientPSMoveAPIEvent event,
+    ClientPSMoveAPI::t_event_data_handle opaque_event_handle)
+{
+    bool bHandled = false;
+
+    switch (event)
+    {
+    case ClientPSMoveAPI::hmdListUpdated:
+        {
+            bHandled = true;
+            request_hmd_list();
+        } break;
     }
 
-    ImGui::End();
+    return bHandled;
+}
+
+void AppStage_HMDSettings::request_hmd_list()
+{
+    if (m_menuState != AppStage_HMDSettings::pendingHmdListRequest)
+    {
+        m_menuState = AppStage_HMDSettings::pendingHmdListRequest;
+        m_selectedHmdIndex = -1;
+        m_hmdInfos.clear();
+
+        // Tell the psmove service that we we want a list of HMDs connected to this machine
+        RequestPtr request(new PSMoveProtocol::Request());
+        request->set_type(PSMoveProtocol::Request_RequestType_GET_HMD_LIST);
+
+        ClientPSMoveAPI::send_opaque_request(&request, AppStage_HMDSettings::handle_hmd_list_response, this);
+    }
+}
+
+void AppStage_HMDSettings::handle_hmd_list_response(
+    ClientPSMoveAPI::eClientPSMoveResultCode ResultCode,
+    const ClientPSMoveAPI::t_request_id request_id,
+    ClientPSMoveAPI::t_response_handle response_handle,
+    void *userdata)
+{
+    AppStage_HMDSettings *thisPtr = static_cast<AppStage_HMDSettings *>(userdata);
+
+    switch (ResultCode)
+    {
+    case ClientPSMoveAPI::_clientPSMoveResultCode_ok:
+        {
+            const PSMoveProtocol::Response *response = GET_PSMOVEPROTOCOL_RESPONSE(response_handle);
+
+            for (int hmd_index = 0; hmd_index < response->result_hmd_list().hmd_entries_size(); ++hmd_index)
+            {
+                const auto &HmdResponse = response->result_hmd_list().hmd_entries(hmd_index);
+
+                AppStage_HMDSettings::HMDInfo HmdInfo;
+
+                HmdInfo.HmdID = HmdResponse.hmd_id();
+
+                switch (HmdResponse.hmd_type())
+                {
+                case PSMoveProtocol::HMDType::OculusDK2:
+                    HmdInfo.HmdType = AppStage_HMDSettings::OculusDK2;
+                    break;
+                default:
+                    assert(0 && "unreachable");
+                }
+
+                HmdInfo.DevicePath = HmdResponse.device_path();
+
+                thisPtr->m_hmdInfos.push_back(HmdInfo);
+            }
+
+            thisPtr->m_selectedHmdIndex = (thisPtr->m_hmdInfos.size() > 0) ? 0 : -1;
+            thisPtr->m_menuState = AppStage_HMDSettings::idle;
+        } break;
+
+    case ClientPSMoveAPI::_clientPSMoveResultCode_error:
+    case ClientPSMoveAPI::_clientPSMoveResultCode_canceled:
+        {
+            thisPtr->m_menuState = AppStage_HMDSettings::failedHmdListRequest;
+        } break;
+    }
 }
--- src/psmoveconfigtool/AppStage_HMDSettings.h
+++ src/psmoveconfigtool/AppStage_HMDSettings.h
@@ -8,7 +8,28 @@
 class AppStage_HMDSettings : public AppStage
 {
 public:
+    enum eHMDType
+    {
+        OculusDK2
+    };
+
+    struct HMDInfo
+    {
+        int HmdID;
+        eHMDType HmdType;
+        std::string DevicePath;
+    };
+
+
     AppStage_HMDSettings(class App *app);
+
+    inline const HMDInfo *getSelectedTrackerInfo() const
+    {
+        return
+            (m_selectedHmdIndex != -1)
+            ? &m_hmdInfos[m_selectedHmdIndex]
+            : nullptr;
+    }
 
     virtual void enter() override;
     virtual void exit() override;
@@ -18,6 +39,33 @@ public:
     virtual void renderUI() override;
 
     static const char *APP_STAGE_NAME;
+
+protected:
+    virtual bool onClientAPIEvent(
+        ClientPSMoveAPI::eClientPSMoveAPIEvent event,
+        ClientPSMoveAPI::t_event_data_handle opaque_event_handle) override;
+
+    void request_hmd_list();
+    static void handle_hmd_list_response(
+        ClientPSMoveAPI::eClientPSMoveResultCode ResultCode,
+        const ClientPSMoveAPI::t_request_id request_id,
+        ClientPSMoveAPI::t_response_handle response_handle,
+        void *userdata);
+
+private:
+    enum eHmdMenuState
+    {
+        inactive,
+        idle,
+
+        pendingHmdListRequest,
+        failedHmdListRequest,
+    };
+    eHmdMenuState m_menuState;
+
+    std::vector<HMDInfo> m_hmdInfos;
+
+    int m_selectedHmdIndex;
 };
 
 #endif // APP_STAGE_HMD_SETTINGS_H
