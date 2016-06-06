e3a32b248fc2d0fdc6c8a95f8eec5960b3c64996
HipsterSloth <hipstersloth908@gmail.com>
2016-06-06 11:32:33 -0700

    Don't check in SetDriverVars.bat. That's supposed to be a locally generated file.

--- src/openvr_plugin/SetDriverVars.bat
+++
@@ -1,5 +0,0 @@
-@echo off 
-set PLATFORM=win32
-set INSTALL_DIR=E:\Program Files (x86)\Steam\steamapps\common\OpenVR\drivers\psmove
-set BUILD_DIR=..\..\win32\bin
-set STEAMVR_RUNTIME_DIR=E:\Program Files (x86)\Steam\steamapps\common\OpenVR
