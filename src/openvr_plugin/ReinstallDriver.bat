e26b614a70c9152bf62456e21779ce406138dfc1
HipsterSloth <hipstersloth908@gmail.com>
2016-06-06 17:50:44 -0700

    Updating the install location for the controller and ps3eye resources

--- src/openvr_plugin/ReinstallDriver.bat
+++ src/openvr_plugin/ReinstallDriver.bat
@@ -6,7 +6,7 @@ IF NOT EXIST "%INSTALL_DIR%\bin\%PLATFORM%" mkdir "%INSTALL_DIR%\bin\%PLATFORM%"
 copy %BUILD_DIR%\driver_psmoveservice.dll "%INSTALL_DIR%\bin\%PLATFORM%\driver_psmove.dll"
 copy %BUILD_DIR%\PSMoveClient.dll "%INSTALL_DIR%\bin\%PLATFORM%"
 "%STEAMVR_RUNTIME_DIR%\bin\win32\vrpathreg" adddriver "%INSTALL_DIR%"
-xcopy /s /i /y "resources" "%STEAMVR_RUNTIME_DIR%\resources"
+xcopy /s /i /y "resources" "%STEAMVR_RUNTIME_DIR%\drivers\psmove\resources"
 
 echo "Done"
 pause
