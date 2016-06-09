8bbd9db4192a26a8deab9150590504fbf77f8386
HipsterSloth <hipstersloth908@gmail.com>
2016-06-09 11:48:25 -0700

    Adding missing call to vrpathreg to remove driver in SteamVR_UninstallDriver.bat

--- src/openvr_plugin/SteamVR_UninstallDriver.bat
+++ src/openvr_plugin/SteamVR_UninstallDriver.bat
@@ -3,6 +3,7 @@ call SteamVR_SetDriverVars.bat
 
 echo "Unstalling PSMoveService SteamVR driver..."
 IF NOT EXIST "%INSTALL_DIR%" goto done
+"%STEAMVR_RUNTIME_DIR%\bin\win32\vrpathreg" removedriver "%INSTALL_DIR%"
 del /F /S /Q "%INSTALL_DIR%*.*"
 rmdir /S /Q "%INSTALL_DIR%"
 
