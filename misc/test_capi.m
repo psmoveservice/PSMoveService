63da247d552920299d462106c62b44e61f3a8782
Chadwick Boulay <chadwick.boulay@gmail.com>
2016-07-11 23:14:18 -0400

    Renamed misc/matlab_psmove folder to misc/matlab_ellipsefit

--- misc/test_capi.m
+++ misc/test_capi.m
@@ -1,22 +1,28 @@
 incl_path = fullfile(pwd, '..', 'src', 'psmoveclient');
 addpath(incl_path);
 
-%if strcmpi(computer(), 'MACI64')
 lib_path = fullfile(pwd, '..', 'build', 'src', 'psmoveclient', 'Debug');
 lib_ext = 'dylib';
 %if strcmpi(computer(), 'WIN64')
 %lib_path = 
 %lib_ext = 'dll'
-
+%end
 addpath(lib_path);
 
+% s_psmcontrollerlist = struct('controller_id', -1, 'controller_type', -1, 'count', 0);
+%%
 loadlibrary(fullfile(lib_path, ['libPSMoveClient_CAPI.', lib_ext]), fullfile(incl_path, 'PSMoveClient_CAPI.h'),...
     'addheader', fullfile(incl_path, 'PSMoveClient_export.h'),...
-    'alias', 'psm');
-
-libfunctionsview('psm')
-
+    'alias', 'psm',...
+    'mfilename', 'psm_proto');
+% libfunctionsview('psm')
 calllib('psm','PSM_Initialize', 'localhost', '9512');
+
+test = calllib('psm', 'PSM_GetController', 0);
+setdatatype(test, 's_PSMControllerPtr', 1)
+
+s_cl = libpointer('s_PSMControllerList');
+calllib('psm','PSM_GetControllerList', s_cl);
 
 % controllers = (PSMController **)malloc(sizeof(PSMController *)*PSMOVESERVICE_MAX_CONTROLLER_COUNT);
 % int controller_count = PSM_GetControllerList(controllers);
@@ -31,5 +37,5 @@ calllib('psm','PSM_Initialize', 'localhost', '9512');
 %PSM_StopControllerDataStream(controllers[ctrl_ix]);
 
 calllib('psm','PSM_Shutdown');
-
+clear s_cl
 unloadlibrary('psm');
