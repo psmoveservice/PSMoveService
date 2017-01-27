dd33c3b85d192e5cc3956d40330f2d82f745ee51
HipsterSloth <hipstersloth908@gmail.com>
2017-01-27 00:57:30 -0800

    Adding psmoveservice apptray stub

---
+++ src/apptray/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by psmove_apptray.rc
+//
+#define IDS_TIP                         1
+#define IDI_ICON1                       101
+#define IDI_TRAYICON                    101
+#define IDR_POPUP                       102
+#define IDD_CONFIGURATION               103
+#define IDR_CONFIGURATION_MENU          106
+#define IDD_CONFIG_SERVICE              107
+#define IDD_ABOUT                       108
+#define IDC_CODEPAGE                    1000
+#define IDC_TEXT                        1001
+#define IDC_REFRESH                     1002
+#define IDC_ABOUT_LOGO                  1003
+#define IDC_QUICK_LOG_TITLE             1005
+#define IDC_QUICK_LOG_TEXT              1006
+#define IDC_ABOUT_PANEL_TEXT            1007
+#define ID_SHOW                         40001
+#define ID_QUIT                         40002
+#define ID_TOOLS_SERVICE                40003
+#define ID_FILE                         40004
+#define ID_SERVICE_CONFIGURE            40005
+#define ID_SERVICE_STOPRUNTIME          40006
+#define ID_SERVICE_STARTRUNTIME         40007
+#define ID_HELP40008                    40008
+#define ID_HELP_ABOUT                   40009
+#define ID_FILE_CLOSE40010              40010
+#define ID_FILE_CLOSE_CONFIGURATION     40011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40012
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
