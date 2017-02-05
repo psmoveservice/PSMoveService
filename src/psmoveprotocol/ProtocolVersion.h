9f999197257d771738235b4a2c56b68631fbe708
HipsterSloth <hipstersloth908@gmail.com>
2017-02-05 01:16:26 -0800

    Bumping build version

--- src/psmoveprotocol/ProtocolVersion.h
+++ src/psmoveprotocol/ProtocolVersion.h
@@ -13,7 +13,7 @@
 #define PSM_PHASE           alpha
 #define PSM_MINOR_VERSION   6
 #define PSM_PATCH_VERSION   0
-#define PSM_BUILD_NUMBER    9
+#define PSM_BUILD_NUMBER    10
 
 /// "Product.Major-Phase Minor.Patch"
 #if !defined(PSM_VERSION_STRING)
