8b1613ba271823324056700caa334c38cf35946e
zelmon64 <zelmon64@gmail.com>
2017-03-02 18:30:48 +0000

    Modified the cross hairs shrink along with the size of the tracked orb
    The cross hairs are now bound by the size of the marker they are drawn
    within.
    
    Changed the numbering of the trackers in the verification step of tracker
    pose calibration because it was the only the had a "+1" so that it didn't
    start at zero like normal.
    
    Also added buttons to the test tracking video feeds to more easily switch
    between them.

--- src/psmoveservice/Device/View/ServerTrackerView.cpp
+++ src/psmoveservice/Device/View/ServerTrackerView.cpp
@@ -613,7 +613,9 @@ public:
 		const cv::Point2f massCenter = computeSafeCenterOfMassForContour<t_opencv_int_contour>(contour);
         cv::drawContours(*bgrShmemBuffer, contours, 0, cv::Scalar(255, 255, 255));
         cv::rectangle(*bgrShmemBuffer, cv::boundingRect(contour), cv::Scalar(255, 255, 255));
-        cv::drawMarker(*bgrShmemBuffer, massCenter, cv::Scalar(255, 255, 255));
+		cv::drawMarker(*bgrShmemBuffer, massCenter, cv::Scalar(255, 255, 255), 0,
+			(cv::boundingRect(contour).height < cv::boundingRect(contour).width) ?
+			cv::boundingRect(contour).height : cv::boundingRect(contour).width);
     }
     
     void
@@ -641,7 +643,8 @@ public:
 					ell_size,
 					pose_projection.shape.ellipse.angle,
 					0, 360, cv::Scalar(0, 0, 255));
-				cv::drawMarker(*bgrShmemBuffer, ell_center, cv::Scalar(0, 0, 255));
+				cv::drawMarker(*bgrShmemBuffer, ell_center, cv::Scalar(0, 0, 255), 0,
+					(ell_size.height < ell_size.width) ? ell_size.height * 2 : ell_size.width * 2);
 			} break;
 		case eCommonTrackingProjectionType::ProjectionType_LightBar:
 			{
