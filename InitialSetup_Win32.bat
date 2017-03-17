ed0f12cd5259a7023b3034e9c493e7e6ccb3d4f5
HipsterSloth <hipstersloth908@gmail.com>
2017-03-16 21:49:56 -0700

    Fixing InitialSetup_Win32.bat - No longer compile the OpenCV project externally

--- InitialSetup_Win32.bat
+++ InitialSetup_Win32.bat
@@ -28,45 +28,6 @@ echo set BOOST_LIB_PATH=%BOOST_ROOT_PATH%/lib32-msvc-14.0 >> SetBuildVars_Win32.
 :: Add MSVC build tools to the path
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-:: Generate the project files for OpenCV 32-bit
-echo "Creating OpenCV 32-bit project files..."
-pushd %OPENCV_ROOT_PATH%\sources
-del /f /s /q build > nul
-rmdir /s /q build
-mkdir build
-pushd build
-cmake -G "Visual Studio 14 2015" -DBUILD_SHARED_LIBS=OFF -DBUILD_WITH_STATIC_CRT=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DBUILD_DOCS=OFF  -DBUILD_opencv_apps=OFF -DBUILD_opencv_calib3d=ON -DBUILD_opencv_flann=ON -DBUILD_opencv_features2d=ON -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_photo=OFF -DBUILD_opencv_ts=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_java=OFF -DWITH_OPENEXR=OFF -DWITH_FFMPEG=OFF -DWITH_JASPER=OFF -DWITH_TIFF=OFF -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF ..
-popd
-popd
-
-pushd %OPENCV_ROOT_PATH%\sources\build
-:: Compile the DEBUG|Win32 and RELEASE|Win32 builds of OpenCV
-echo "Building OpenCV DEBUG|Win32..."
-MSBuild.exe OpenCV.sln /p:configuration=DEBUG /p:Platform="Win32" /t:Clean;Build 
-echo "Building OpenCV RELEASE|Win32..."
-MSBuild.exe OpenCV.sln /p:configuration=RELEASE /p:Platform="Win32" /t:Clean;Build
-
-:: "Install" the 32-bit debug and release builds of OpenCV into the install/x86 folder
-echo "Installing OpenCV DEBUG|Win32..."
-MSBuild.exe INSTALL.vcxproj /p:configuration=DEBUG /p:Platform="Win32" /t:Build
-echo "Installing OpenCV RELEASE|Win32..."
-MSBuild.exe INSTALL.vcxproj /p:configuration=RELEASE /p:Platform="Win32" /t:Build
-
-:: Clean out any existing %OPENCV_ROOT_PATH%/build/x86 folder
-echo "Cleanout out any existing x86 OpenCV build"
-del /f /s /q %OPENCV_ROOT_PATH%/build/x86 > nul
-rmdir /s /q %OPENCV_ROOT_PATH%/build/x86
-
-:: Move the x86 folder into the top level build folder alongside the x64 build
-echo "Moving OpenCV x86 build adjacent x64 build"
-move install/x86 %OPENCV_ROOT_PATH%/build
-
-:: Clean out build intermediates
-echo "Cleaning out OpenCV x86 build intermediates"
-MSBuild.exe OpenCV.sln /p:configuration=DEBUG /p:Platform="Win32" /t:Clean
-MSBuild.exe OpenCV.sln /p:configuration=RELEASE /p:Platform="Win32" /t:Clean
-popd
-
 :: Compile the DEBUG|Win32 and RELEASE|Win32 builds of protobuf
 echo "Creating protobuf project files..."
 pushd thirdparty\protobuf
