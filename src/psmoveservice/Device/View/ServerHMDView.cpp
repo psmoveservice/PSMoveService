5ad647738580ccd0b0384bc5bbe98941cc392d18
HipsterSloth <hipstersloth908@gmail.com>
2017-05-27 23:38:16 -0700

    Fixed issue with VirtualHMD color configs not getting loaded * Needed to prefix config identifier with "hmd"

--- src/psmoveservice/Device/View/ServerHMDView.cpp
+++ src/psmoveservice/Device/View/ServerHMDView.cpp
@@ -605,7 +605,7 @@ ServerHMDView::getConfigIdentifier() const
 		else if (m_device->getDeviceType() == CommonDeviceState::VirtualHMD)
 		{
             // THe "USB device path" for a Virtual HMD is actually just the Virtual HMD identifier, i.e. "VirtualHMD_0"
-			identifier = m_device->getUSBDevicePath();
+			identifier = "hmd_"+m_device->getUSBDevicePath();
 		}
 		else
 		{
