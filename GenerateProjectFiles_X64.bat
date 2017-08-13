fed2af30de1bec9c9913a0149d56832e78913ff6
HipsterSloth <hipstersloth908@gmail.com>
2017-08-12 21:35:16 -0700

    Adding "BuildOfficialDistribution.bat" batch script for generating official Windows releases * BuildOfficialDistribution.bat does the following:   - Fully Builds Win32/x64 Debug/Release configurations   - Generates distribution zip from the RELEASE|x64 configuration * Updated GenerateProjectFiles_*.bat and InitialSetup_*.bat to handle error return codes * Updated cmake install action in all cmake scripts to output to seperate debug and release folders

--- GenerateProjectFiles_X64.bat
+++ GenerateProjectFiles_X64.bat
@@ -6,4 +6,13 @@ cd build
 
 echo "Rebuilding PSMoveService x64 Project files..."
 cmake .. -G "Visual Studio 14 2015 Win64" -DOpenCV_DIR=%OPENCV_BUILD_PATH% -DBOOST_ROOT=%BOOST_ROOT_PATH% -DBOOST_LIBRARYDIR=%BOOST_LIB_PATH% -DPROTOBUF_SRC_ROOT_FOLDER=..\thirdparty\protobuf
+IF %ERRORLEVEL% NEQ 0 (
+  echo "Error generating PSMoveService 64-bit project files"
+  goto failure
+)
+
+EXIT /B 0
+
+:failure
 pause
+EXIT /B 1
