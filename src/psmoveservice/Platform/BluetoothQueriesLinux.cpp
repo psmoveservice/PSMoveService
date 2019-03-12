90e1e2afccf71bfa23ecdbf9943105aa5f6467ad
Chadwick Boulay <chadwick.boulay@gmail.com>
2019-03-12 13:32:32 -0400

    Linux - get host address (TODO: Error handling).

--- src/psmoveservice/Platform/BluetoothQueriesLinux.cpp
+++ src/psmoveservice/Platform/BluetoothQueriesLinux.cpp
@@ -8,11 +8,22 @@
 #include <assert.h>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
 
 //-- Queries -----
 bool bluetooth_get_host_address(std::string &out_address)
 {
     bool bSuccess = true;
+
+    bdaddr_t btaddr;
+    char pszaddr[18];
+
+    // Assume only one bt adapter.
+    int dev_id = hci_get_route(NULL);
+    hci_devba(dev_id, &btaddr);
+    ba2str(&btaddr, pszaddr);
+    out_address.assign(pszaddr);
+
     return bSuccess;
 }
